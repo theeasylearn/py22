77bb39b9b67e6a5f34a0966f7187f15a3c4642ea
theeasylearn <theeasylearn@gmail.com>
2025-02-03 09:36:56 +0530

    day 4

---
+++ 3_variables.py
@@ -0,0 +1,18 @@
+#concept of variables in python 
+'''
+    syntax 
+    variable-name = value 
+'''
+name = "The easylearn academy"
+year = 2015
+pi = 3.14
+gender = True
+
+print("name = ",name," year = ",year," pi = ",pi," gender = ",gender)
+
+#change variable's value
+name = 100
+year = "two thousand twenty five"
+pi = False
+gender = 1.5 
+print("name = ",name," year = ",year," pi = ",pi," gender = ",gender)
---
+++ 4_input.py
@@ -0,0 +1,8 @@
+#learn how to take input using keyboard
+age = input("What is your age")
+#convert string age into numeric age 
+age = int(age)
+print("your age is ",age)
+#calculate how many days he lived
+days = age * 365 #expression
+print("you have lived ",days," so far")
