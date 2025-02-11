f87ae903375ca30803e1c6b9da6af0064c7f7227
theeasylearn <theeasylearn@gmail.com>
2025-02-11 09:38:16 +0530

    operators

---
+++ 13_arthimetic.py
@@ -0,0 +1,16 @@
+#example of athematic operators 
+num1 = input("enter value for num1")
+num2 = input("enter value for num2")
+
+#convert input into int 
+num1 = int(num1)
+num2 = int(num2)
+
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2 
+division = num1 / num2 
+reminder = num1 % num2 
+floor = num1 // num2 # 10^3
+power = num1 ** num2 
+print(addition,subtraction,multiplication,division,reminder,floor,power)
---
+++ 14_relational.py
@@ -0,0 +1,15 @@
+#example of relational operators
+num1 = input("enter value for num1")
+num2 = input("enter value for num2")
+
+#convert input into int 
+num1 = int(num1) # 10
+num2 = int(num2) # 20
+
+# //relational expression use relational operators < > == != <= >=
+print("num1 == num2",num1 == num2) # false
+print("num1 != num2",num1 != num2) # true
+print("num1 < num2",num1 < num2) # true
+print("num1 > num2",num1 > num2) # false 
+print("num1 <= num2",num1 <= num2) # true 
+print("num1 >= num2",num1 >= num2) # false
