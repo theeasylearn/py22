3e3b6e5207743b749c76d3ce23495a6e117f2c06
theeasylearn <theeasylearn@gmail.com>
2025-02-12 17:27:49 +0530

    set

--- 11_set.py
+++ 11_set.py
@@ -1,17 +1,14 @@
 #concept of set 
-fruits = {'graps','kiwi','orange'}
+fruits = {'Apple','Banana','Mango','kiwi','graps'}
 print(fruits)
-fruits.add('apple')
-fruits.add('kiwi')
-fruits.add('graps')
-print(fruits)
-fruits.remove('kiwi')
+fruits.add('Apple')
+fruits.add('coconut')
+fruits.remove('Apple')
 print(fruits)
 
-set1 = {1,5,8,11}
-set2 = {8,11,25,45}
+set1 = {1,2,3}
+set2 = {2,3,4}
 print(set1,set2)
-
 union = set1.union(set2)
 intersection = set1.intersection(set2)
 difference = set1.difference(set2)
--- 9_dictionary.py
+++ 9_dictionary.py
@@ -1,24 +1,28 @@
 #concept of dictionary
-person = ['Gorbachav','Mikhile',True,40,True]
-person = {'name':"Ankit",'surname':"Patel",'age':38,'gender':True,'secret':None}
-print(person)
-print(person['name'])
-person['name'] = "ANKIT"
-del person['secret']
-print(person)
+# person = ['Gorbachav','Mikhile',True,40,True]
+# person = {'name':"Ankit",'surname':"Patel",'age':38,'gender':True,'secret':None}
+# print(person)
+# print(person['name'])
+# person['name'] = "ANKIT"
+# del person['secret']
+# print(person)
 
-books = {} #empty dictionary
-books['name'] = "The Atomic habit"
-books['author'] = "James clear"
-books['chapters'] = (1,2,3,4,5)
-books['topics'] = ['index','preface','introduction']
-print(books)
-#ger first chapter 
-print(books['chapters'][0])
-#get second topic 
-print(books['topics'][1])
-movies = [] #empty list
-movies.append({'name':'abc','year':2025}) #add dictionary into list
-movies.append({'name':'xyz','year':2026}) #add dictionary into list
-movies.append({'name':'pqr','year':2027}) #add dictionary into list
+book = {}
+book['name'] = 'Atomic Habit'
+book['price'] = 1000
+book['author'] = "James clear"
+#add tuples into dictionary
+book['chapters'] = (1,2,3,4)
+#add list into dictionary 
+book['topics'] = ['Energy','focus','hard work']
+print(book)
+
+movies = []
+movie1 = {'name':'abc','year':2023}
+movie2 = {'name':'ghi','year':2024}
+movie3 = {'name':'xyz','year':2025}
+#add dictionary into list
+movies.append(movie1)
+movies.append(movie2)
+movies.append(movie3)
 print(movies)
