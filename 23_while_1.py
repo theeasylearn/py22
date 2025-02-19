acbbe9589c34ce4e6f1e33d6678b28ea2255fcc6
theeasylearn <theeasylearn@gmail.com>
2025-02-19 09:50:13 +0530

    while loop day 1

---
+++ 23_while_1.py
@@ -0,0 +1,10 @@
+# write a program to print following pattern
+# 1 3 5 7 9 ... 100
+count = 1
+
+while count<100:
+    print(count)
+    count = count + 2
+
+print("Good bye...")
+
---
+++ 24_while_2.py
@@ -0,0 +1,10 @@
+# write a program to print following pattern
+# 1 8 27 64 125 .... 1000
+
+count = 1
+while count<=10:
+    print(count*count*count,end=' ') #1 
+    count+=1 #2 
+print('')
+print("Good bye...")
+
